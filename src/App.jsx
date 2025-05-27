9f5d74c80b19caa6004d28f8152c56160dd1a8ab
Chico204 <olatunjimark391@gmail.com>
2025-05-27 02:24:21 +0100

    almost

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import data from './data'
+
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
 
+  const handleSubmit = (e) => {
+   e.preventDefault()
+    let amount = parseInt(count)
+    if(count<= 0){
+      amount= 1
+    }
+    if(count >= 8){
+      amount= 8
+    }
+
+
+    setText(data.slice(0, amount))
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <section className='section-center py-5 px-0 align-center'>
+    <h3 className='text-3xl text-center pt-[100px] mb-2'>Tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor="amount " className='text-xl'>Paragraphs:</label>
+      <input type="number" id='amount' value={count}  onChange ={(e)=> setCount(e.target.value)} />
+      <button className='btn'>Generate</button>
+    </form>
+    <article className='lorem-text'>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>
+      })}
+    </article>
+   </section>
   )
 }
 
